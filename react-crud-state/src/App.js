1a0c732c6f6c4b7f54f16820e794313a815f2889
tutv <tutv@egdgroup.com>
2021-10-13 18:53:43 +0700

    add

--- react-crud-state/src/App.js
+++ react-crud-state/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import Books from "./Component/Books";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className="App">
+      <h1>Book Manager</h1>
       <Books />
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
--- react-crud-state/src/Component/Books.js
+++ react-crud-state/src/Component/Books.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 import AddBook from "./Add";
+import { toast } from "react-toastify";
 class Book extends Component {
   state = {
     Books: [
       { id: 1, title: "I love you so" },
-      { id: 2, title: "You!!!" },
+      { id: 2, title: "My dad love me" },
+      { id: 3, title: "You!!!" },
     ],
   };
 
-  addNewBook = (todo) => {
+  addNewBook = (book) => {
     this.setState({
-      Books: [...this.state.Books, todo],
+      Books: [...this.state.Books, book],
+    });
+    toast.success("Thêm Mới Thành Công");
+  };
+
+  handleDeleteBook = (book) => {
+    // console.log(book);
+    let currentBook = this.state.Books;
+    currentBook = currentBook.filter((item) => item.id !== book.id);
+    this.setState({
+      Books: currentBook,
+    });
+    toast.success("Xóa Thành Công");
+  };
+
+  handleEditBook = (book) => {
+    this.setState({
+      editBook: book,
     });
   };
+
   render() {
     let { Books } = this.state;
+
     return (
       <div className="list-book-container">
         <AddBook addNewBook={this.addNewBook} />
@@ -24,11 +45,24 @@ class Book extends Component {
             Books.map((item, index) => {
               return (
                 <div className="book-child" key={item.id}>
-                  <span>
-                    {index + 1} - {item.title}
-                  </span>
-                  <button className="edit">Edit</button>
-                  <button className="delete">Delete</button>
+                  <button> {index + 1} </button>
+
+                  <input value={item.title} />
+
+                  <button
+                    className="edit"
+                    onClick={() => this.handleEditBook(item)}
+                  >
+                    {" "}
+                    Edit{" "}
+                  </button>
+                  <button
+                    className="delete"
+                    onClick={() => this.handleDeleteBook(item)}
+                  >
+                    {" "}
+                    Delete
+                  </button>
                 </div>
               );
             })}
